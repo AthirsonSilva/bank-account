7d91f7988a4eeaa4e9c1f13b4219665645a1acab
athir <84593887+AthirsonSilva@users.noreply.github.com>
2022-06-22 17:45:11 -0300

    Add files via upload

--- src/DAO/ClientDao.java
+++ src/DAO/ClientDao.java
@@ -1,6 +1,7 @@
 package DAO;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
@@ -15,7 +16,16 @@ public class ClientDao {
 	
 	public void register(Client client) throws SQLException {
 		try {
-			String sql = "INSERT INTO Cliente(name, cpf, email)";
+			String sql = "INSERT INTO Cliente(name, cpf, email)"
+						+ "VALUES (?, ?, ?)";
+			
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, client.getClientName());
+			stmt.setString(1, client.getClientCpf());
+			stmt.setString(1, client.getClientEmail());
+			
+			stmt.execute();
+			stmt.close();
 		} finally {
 			conn.close();
 		}
